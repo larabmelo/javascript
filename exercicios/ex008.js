2277382e19bd97720c97a4fae2754fb5cab1b567
Lara Melo <larabmelo@outlook.com>
2023-04-05 20:28:55 +0200

    condicionais em js ate ex010

---
+++ exercicios/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condicao simples, pois nao tem "se nao", se passa abaixo da velocidade, vc n ganha um parabens kkkk
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`) 
---
+++ exercicios/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') { //diferente de brasil
+    console.log('Voce é Brasileiro!')
+} else {
+    console.log('Voce é estrangeiro!')
+}
