92d35241deb9425bdc25f69b3ccbaec0b7d1f8ab
Lara Melo <larabmelo@outlook.com>
2023-04-18 19:19:19 +0200

    desafio001 feito

--- desafios/desafio001/script.js
+++ desafios/desafio001/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours() 
+    var back = document.querySelector("body#fundo")
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+            img.src = 'foto-manha.png'
+            back.style.backgroundColor = '#659be1'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE
+            img.src = 'foto-tarde.png'
+            back.style.backgroundColor = '#977b53'
+    } else { 
+        // BOA NOITE
+            img.src = 'foto-noite.png'
+            back.style.backgroundColor = '#36363e'
+    }
+}
