bafe8cdc9552895890cfd04765ca5c4911629c6c
Lara Melo <larabmelo@outlook.com>
2023-04-19 16:44:03 +0200

    desafio002 jscript

--- desafios/desafio002/script.js
+++ desafios/desafio002/script.js
@@ -4,10 +4,44 @@ function verificar() {
     var fano = window.document.getElementById('txtano')
     var res = window.document.querySelector('div#res')
 
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
     window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-       
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       if (fsex[0].checked) {
+           genero = 'homem'
+           if (idade >= 0 && idade < 10) {
+               // CRIANCA
+
+           } else if (idade >= 10 && idade < 27) {
+               // JOVEM
+
+           } else if (idade >= 27 && idade < 50) {
+               // ADULTO
+           } else {
+               // IDOSO
+           }
+
+       } else if (fsex[1].checked) {
+           genero = 'mulher'
+           if (idade >= 0 && idade < 10) {
+            // CRIANCA
+
+        } else if (idade >= 10 && idade < 27) {
+            // JOVEM
+
+        } else if (idade >= 27 && idade < 50) {
+            // ADULTO
+        } else {
+            // IDOSO
+        }
+       }
+       //res.style.textAlign = 'center'
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
 
