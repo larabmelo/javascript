cf5b84bd45962a8678b95db850f00e13061e3ed7
Lara Melo <larabmelo@outlook.com>
2023-05-04 15:56:37 +0200

    ex015 js

---
+++ exercicios/ex015.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+console.log('Vai comecar..')
+for (var c = 1;c <= 5;c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+//depois da variavel e fazer o teste logico, é verdadeiro e vai executar o comando, após o comando, o incremento é feito e tudo se repete
